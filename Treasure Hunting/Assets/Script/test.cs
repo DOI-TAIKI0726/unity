b317bd24492d76cf59a33683d94e9cf616d461a0
DOI-TAIKI0726 <74754698+DOI-TAIKI0726@users.noreply.github.com>
2022-10-04 15:27:34 +0900

    tssut

---
+++ Treasure Hunting/Assets/Script/test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    void Start()
+    {
+        int chinko = 1;
+        if (chinko == 1)
+        {
+            chinko = 2;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
