d37df3a5e747125fcbfb508fed1c613911ef2bbf
FUJITA0928 <201071@st.yoshida-g.ac.jp>
2022-10-07 08:54:19 +0900

    rotarion

--- Treasure Hunting/Assets/Scripts/Game/RotateSwitch.cs
+++ Treasure Hunting/Assets/Scripts/Game/RotateSwitch.cs
@@ -6,31 +6,20 @@ public class RotateSwitch : MonoBehaviour
 {
     //回転するモデル
     GameObject obj1;
-    GameObject obj2;
-    GameObject obj3;
+    
 
     //スイッチ名
     [SerializeField]
     private string switchName1 = "switch1";
-    [SerializeField]
-    private string switchName2 = "switch2";
-    [SerializeField]
-    private string switchName3 = "switch3";
-
+    
     //回転するオブジェクトの名前
     [SerializeField]
     private string objName1 = "rotateCube1";
-    [SerializeField]
-    private string objName2 = "rotateCube2";
-    [SerializeField]
-    private string objName3 = "rotateCube3";
 
     void Start()
     {
         //モデル名取得
         obj1 = GameObject.Find(objName1);
-        obj2 = GameObject.Find(objName2);
-        obj3 = GameObject.Find(objName3);
     }
 
     // Update is called once per frame
@@ -51,14 +40,7 @@ public class RotateSwitch : MonoBehaviour
                 {
                     obj1.transform.Rotate(0, 90, 0);
                 }
-                if (gameObject.name == switchName2)
-                {
-                    obj2.transform.Rotate(0, 90, 0);
-                }
-                else if (gameObject.name == switchName3)
-                {
-                    obj3.transform.Rotate(0, 90, 0);
-                }
+                
             }
         }
     }
