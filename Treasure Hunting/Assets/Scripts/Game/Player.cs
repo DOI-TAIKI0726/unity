438acf1f6ea1626c7485e85647b1ab74abf459f6
Terashita-Rui <201009@st.yoshida-g.ac.jp>
2022-10-07 14:33:18 +0900

    Playerとカメラ動かせるようにした

---
+++ Treasure Hunting/Assets/Scripts/Game/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //移動速度
+    [SerializeField]
+    private float moveSpeed;
+
+    //カメラの正面方向
+    private Vector3 cameraForward;
+    //移動方向ベクトル
+    private Vector3 moveVector;
+    //リジットボディ参照
+    private Rigidbody rigidBody;
+
+    void Start()
+    {
+        rigidBody = this.GetComponent<Rigidbody>();
+
+    }
+
+    void FixeUpdate()
+    {
+        //移動キーの入力を取得
+        //縦
+        float horizonal = Input.GetAxis("Horizontal");
+        //横
+        float vertical = Input.GetAxis("Vertical");
+
+        //カメラの方向からXZ平面の単位ベクトル取得
+        cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        //移動方向ベクトルを設定
+        moveVector = cameraForward * vertical + Camera.main.transform.right * horizonal;
+
+        //移動
+        rigidBody.AddForce(new Vector3(moveVector.x, 0, moveVector.z) * moveSpeed, ForceMode.Force);
+    }
+}
