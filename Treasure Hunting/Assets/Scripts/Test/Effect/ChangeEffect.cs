166843abc335a38f67167c44e4b96022f1ccbeea
DOI-TAIKI0726 <74754698+DOI-TAIKI0726@users.noreply.github.com>
2022-10-13 13:06:19 +0900

    エフェクトを切り替え出来るように

---
+++ Treasure Hunting/Assets/Scripts/Test/Effect/ChangeEffect.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeEffect : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject buff;
+    [SerializeField]
+    private GameObject confetti;
+    [SerializeField]
+    private GameObject Kirakira;
+    [SerializeField]
+    private int Count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        buff = GameObject.Find("Buff");
+        confetti = GameObject.Find("Confetti");
+        Kirakira = GameObject.Find("KiraKira");
+
+        Count = 0;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("t"))
+        {
+            Count++;
+            if (Count >= 3)
+            {
+                Count = 0;
+            }
+        }
+        switch (Count)
+        {
+            case 0:
+                buff.SetActive(true);
+                confetti.SetActive(false);
+                Kirakira.SetActive(false);
+                break;
+            case 1:
+                buff.SetActive(false);
+                confetti.SetActive(true);
+                Kirakira.SetActive(false);
+                break;
+            case 2:
+                buff.SetActive(false);
+                confetti.SetActive(false);
+                Kirakira.SetActive(true);
+                break;
+        }
+    }
+}
