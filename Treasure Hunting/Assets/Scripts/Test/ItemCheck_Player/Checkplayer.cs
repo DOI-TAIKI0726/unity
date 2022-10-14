2435d69b9773a1fd3716698fcc7a001b53384843
yuto05 <201016@st.yoshida-g.ac.jp>
2022-10-14 11:39:28 +0900

    RandomItemの更新、CheckPlayerの更新、コメントの編集

--- Treasure Hunting/Assets/Scripts/Test/ItemCheck_Player/Checkplayer.cs
+++ Treasure Hunting/Assets/Scripts/Test/ItemCheck_Player/Checkplayer.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 public class Checkplayer : MonoBehaviour
 {
     //移動
-    float x;                                //x方向の移動
-    float z;                                //y方向の移動
-    public float WalkSpeed = 3.0f;          //移動の速さ
-    public float gravity = 9.8f;            //重力
-    Rigidbody rb;                           //リジッドボディ
-
-    //確認用
-    private int ItemCount = 0;              //アイテム数
+    private float x;                                //x方向の移動
+    private float z;                                //z方向の移動
+    public float WalkSpeed = 3.0f;                  //移動の速さ
+    public float gravity = 9.8f;                    //重力
+    private Rigidbody rb;                           //リジッドボディ
+    
+    //アイテム吐き出す場合必要な変数
+    //ここから
     //アイテム
     [SerializeField]
-    private GameObject Item = null;
+    private GameObject[] Item = null;
+    //oldtypeのデータの数
+    [SerializeField]
+    private int DataCnt = 0;
+    private int Itemtype;                           //取得したアイテムの種類
+    private int[] oldtype;                          //上記の前に取得したアイテムの種類
+    //確認用
+    private int ItemCount = 0;              //アイテム数
+    //ここまで
 
     // Start is called before the first frame update
     void Start()
     {
         //リジッドボディの情報の格納
         rb = GetComponent<Rigidbody>();
+        //oldtypeのデータ数の更新
+        oldtype = new int[DataCnt];
     }
 
     // Update is called once per frame
@@ -39,14 +49,31 @@ public class Checkplayer : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space))
         {
             //アイテム数が0より大きい場合
-            if(ItemCount>0)
+            if (ItemCount > 0)
             {
                 //減算
                 ItemCount = GameObject.Find("GameManager").GetComponent<ItemCheck>().Add(-1);
 
                 //アイテムの生成
+                //生成する位置を取得
                 Vector3 position = new Vector3(transform.position.x + 2f, transform.position.y, transform.position.z);
-                GameObject NewItem = Instantiate(Item, position, transform.rotation);
+                //生成
+                GameObject NewItem = Instantiate(Item[Itemtype], position, transform.rotation);
+                
+                //次生成するアイテムの更新
+                for (int cnt = 0; cnt < oldtype.Length; cnt++)
+                {
+                    //次に生成するアイテムの種類を取得
+                    if (cnt == 0)
+                    {
+                        Itemtype = oldtype[cnt];
+                    }
+                    //生成するアイテムの順番の更新
+                    else
+                    {
+                        oldtype[cnt - 1] = oldtype[cnt];
+                    }
+                }
             }
         }
         //ここまで
@@ -64,6 +91,48 @@ public class Checkplayer : MonoBehaviour
 
             //加算
             ItemCount = GameObject.Find("GameManager").GetComponent<ItemCheck>().Add(1);
+
+            //アイテムを吐き出す場合必要な処理
+            //ここから
+            //取得したアイテムの種類を取得
+            for (int cnt = 0; cnt < Item.Length; cnt++) 
+            {
+                if (col.gameObject.name == Item[cnt].name || col.gameObject.name == Item[cnt].name + "(Clone)")
+                {
+                    if(ItemCount>0)
+                    {
+                        for (int i = oldtype.Length - 1; i >= 0; i--) 
+                        {
+                            //oldtypeの更新
+                            if (i == 0)
+                            {
+                                //oldtype[0]に現在のItemtypeを代入
+                                oldtype[i] = Itemtype;
+                                Debug.Log(oldtype[i]);
+                            }
+                            else
+                            {
+                                //1つ前のデータを現在のデータに代入
+                                oldtype[i] = oldtype[i - 1];
+                            }
+                        }
+                    }
+                    
+                    //Itemtypeの更新
+                    Itemtype = cnt;
+                    break;
+                }
+            }
+            //ここまで
+        }
+        //当たったオブジェクトのタグがRouletteの場合
+        if(col.gameObject.tag=="Roulette")
+        {
+            //アイテムの削除
+            Destroy(col.gameObject);
+
+            //ルーレット開始の処理
+            GameObject.Find("GameManager").GetComponent<RandomItem>().RouletteStart();
         }
     }
 }
