75c4c272df12a4b738c89505ff08a0423e0d21f7
yuto05 <201016@st.yoshida-g.ac.jp>
2022-12-15 13:34:47 +0900

    道中の障害物の更新

--- Treasure Hunting/Assets/Main/Scripts/Test/MoveObstacle.cs
+++ Treasure Hunting/Assets/Main/Scripts/Test/MoveObstacle.cs
@@ -11,7 +11,14 @@ public class MoveObstacle : MonoBehaviour
     //スピード
     [SerializeField]
     private float speed;
+    //跳ね返すときの速さ
+    [SerializeField]
+    private float boundSpeed;
+    //跳ね返す単位ベクトルにかける倍数
+    [SerializeField]
+    private float bounceVector;
 
+    //始まりの位置
     private Vector3 StartPos;
 
     // Start is called before the first frame update
@@ -27,5 +34,21 @@ public class MoveObstacle : MonoBehaviour
         //移動
         //始まりの位置からmove分移動する
         transform.position = new Vector3((Mathf.Sin((Time.time) * speed) * move.x + StartPos.x), (Mathf.Sin((Time.time) * speed) * move.y + StartPos.y), (Mathf.Sin((Time.time) * speed) * move.z + StartPos.z));
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            //跳ね返りの処理
+            //衝突した面の、接触した点における法線ベクトルを取得
+            Vector3 normal = col.contacts[0].normal;
+            //衝突した速度ベクトルを単位ベクトルにする
+            Vector3 velocity = col.rigidbody.velocity.normalized;
+            //x,z方向に対して逆向きの法線ベクトルを取得
+            velocity += new Vector3(-normal.x * bounceVector, 0f, -normal.z * bounceVector);
+            //取得した法線ベクトルに跳ね返す速さをかけて、跳ね返す
+            col.rigidbody.AddForce(velocity * boundSpeed, ForceMode.Impulse);
+        }
     }
 }
