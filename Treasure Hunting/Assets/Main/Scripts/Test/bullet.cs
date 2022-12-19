1f3b0400de946bd57248d654160cd9d8e9f7ba61
FUJITA0928 <74754792+FUJITA0928@users.noreply.github.com>
2022-12-19 14:10:46 +0900

    mapの更新、弾を発射するスクリプト追加

---
+++ Treasure Hunting/Assets/Main/Scripts/Test/bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    //跳ね返すときの速さ
+    [SerializeField]
+    private float boundSpeed;
+    //跳ね返す単位ベクトルにかける倍数
+    [SerializeField]
+    private float bounceVector;
+    
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            //跳ね返りの処理
+            //衝突した面の、接触した点における法線ベクトルを取得
+            Vector3 normal = col.contacts[0].normal;
+            //衝突した速度ベクトルを単位ベクトルにする
+            Vector3 velocity = col.rigidbody.velocity.normalized;
+            //x,z方向に対して逆向きの法線ベクトルを取得
+            velocity += new Vector3(-normal.x * bounceVector, 0f, -normal.z * bounceVector);
+  
+            //取得した法線ベクトルに跳ね返す速さをかけて、跳ね返す
+            col.rigidbody.AddForce(velocity * boundSpeed, ForceMode.VelocityChange);
+        }
+    }
+}
---
+++ Treasure Hunting/Assets/Main/Scripts/Test/bulletCreate.cs
@@ -0,0 +1,62 @@
+﻿using UnityEditor;
+using UnityEngine;
+//砲弾等の射出物の生成管理
+
+
+public class bulletCreate : MonoBehaviour
+{
+    //発射感覚
+    private int count;
+    private bool randomStart;
+    private float fireTime;//発射時間
+    GameObject bulletprefab;//弾Obj
+    GameObject ItemBoxprefab;//ItemBoxObj
+    //GameManagerスクリプト
+    private GameManager gameManagerScript;
+
+    void Start()
+    {
+        // 任意のフォルダから読み込む
+        bulletprefab = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Main/Prefabs/test/bullet.prefab", typeof(GameObject));
+        ItemBoxprefab = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Main/Prefabs/test/ReSpoonItemBox.prefab", typeof(GameObject));
+
+
+        gameManagerScript = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+    void Update()
+    {
+        if (gameManagerScript.quitPanel.activeSelf == false)
+        {
+            //カウント進める
+            count += 1;
+
+            // 3秒ごとに砲弾を発射する
+            if (count >= 180)
+            {
+                count = 0;
+                //打ち出すアイテムを乱数で決める
+                int RandItem = Random.Range(1, 100);
+
+                if (RandItem <= 10)
+                {
+                    GameObject ItemBox = Instantiate(ItemBoxprefab, transform.position, Quaternion.identity);
+                    Rigidbody bullRb = ItemBox.GetComponent<Rigidbody>();
+                    //弾速は自由に設定
+                    bullRb.AddForce(transform.forward * 500);
+                    // 2秒後に砲弾を破壊する
+                    Destroy(ItemBox, 2.0f);
+                }
+                else
+                {
+                    GameObject bullet = Instantiate(bulletprefab, transform.position, Quaternion.identity);
+                    Rigidbody bullRb = bullet.GetComponent<Rigidbody>();
+                    // 弾速は自由に設定
+                    bullRb.AddForce(transform.forward * 500);
+                    // 2秒後に砲弾を破壊する
+                    Destroy(bullet, 2.0f);
+                }
+
+            }
+        }
+    }
+}
