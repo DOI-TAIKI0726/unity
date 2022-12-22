856dbccc1605f24c0a275a4ee6bd01381378bec2
DOI-TAIKI0726 <74754698+DOI-TAIKI0726@users.noreply.github.com>
2022-12-22 09:18:07 +0900

    魔法陣の処理の追加(途中)

---
+++ Treasure Hunting/Assets/Main/Scripts/Test/Effect/MagicCircleController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicCircleController : MonoBehaviour
+{
+    private GameObject ObjMagicCircle;
+    // フェードアウトするまでの時間
+    public float fadeTime;
+    private float time;
+    private SpriteRenderer render;
+    private GameObject ObjPlayer;
+    bool SwitchStepOn;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ObjMagicCircle = GameObject.Find("MagicCircle");
+        render = ObjMagicCircle.GetComponent<SpriteRenderer>();
+        ObjPlayer = GameObject.Find("Player");
+        ObjMagicCircle.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SwitchStepOn == true)
+        {
+            Debug.Log("踏み続けている");
+            time += Time.deltaTime;
+            if (time < fadeTime)
+            {
+                if(ObjMagicCircle.activeSelf == true)
+                {
+                    float alpha = 0.1f + time / fadeTime;
+                    Color color = render.color;
+                    color.a = alpha;
+                    render.color = color;
+                }
+            }
+        }
+        else 
+        {
+            Debug.Log("踏んでない");
+            time += Time.deltaTime;
+            if (time < fadeTime)
+            {
+                //ObjMagicCircle.SetActive(false);
+
+                if(ObjMagicCircle.activeSelf == false)
+                {
+                    float alpha = 1.0f - time / fadeTime;
+                    Color color = render.color;
+                    color.a = alpha;
+                    render.color = color;
+                }
+            }
+        }
+    }
+
+    //踏み続けている間
+    void OnCollisionStay(Collision other)
+    {
+        SwitchStepOn = true;
+    }
+
+    //踏んでいない
+    void OnCollisionExit(Collision other)
+    {
+        SwitchStepOn = false;
+    }
+
+}
+
