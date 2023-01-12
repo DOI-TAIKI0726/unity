b0d3b0b0ab027bb0dfa9bbccd7bf674ec841bc07
yuto05 <201016@st.yoshida-g.ac.jp>
2023-01-12 11:35:48 +0900

    minimap用のAxisを追加

---
+++ Treasure Hunting/Assets/Main/Scripts/Game/Camera/MiniMapAxis.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapAxis : MonoBehaviour
+{
+    //メインカメラのaxis
+    GameObject axis;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Axisの情報を取得
+        axis = GameObject.Find("Axis");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ミニマップのAxisに
+        //メインカメラのAxisの回転Yを代入
+        transform.eulerAngles = new Vector3(90f, axis.transform.eulerAngles.y, 0f);
+    }
+}
